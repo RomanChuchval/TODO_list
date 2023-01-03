709a307aa9458400bb7004b6e917a1266bf2d569
Roman Chuchval <rchuchval@gmail.com>
2023-01-03 11:43:31 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import TodoList, {TaskType} from "./TodoList";
+
+function App() {
+
+    const todoListTitle_1: string = 'What to learn';
+    const todoListTitle_2: string = 'What to buy';
+
+    const tasks_1: Array<TaskType> = [
+        {
+            id: 1,
+            title: 'HTML',
+            isDone: true,
+        },
+        {
+            id: 2,
+            title: 'React',
+            isDone: false,
+        },
+        {
+            id: 3,
+            title: 'TypeScript',
+            isDone: true,
+        },
+    ]
+
+    return (
+        <div className='App'>
+            <TodoList title={todoListTitle_1} tasks={tasks_1}/>
+            {/*<TodoList title={todoListTitle_2} tasks={tasks_1}/>*/}
+        </div>
+    );
+}
+
+export default App;
